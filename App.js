7224c94ae50f23da3b7563507576dd19ce7c46b6
herowalking <hero@2017>
2018-05-19 21:30:56 +0800

    滚动式Banner

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React, {Component} from 'react';
 const BagView = require('./components/BagView');
 const LoginView = require('./components/LoginView');
 const MyScrollView = require('./components/MyScrollView');
+const BannerView = require('./components/BannerView');
 
 export default class first extends Component {
     render() {
         // return <BagView />
         // return <LoginView/>
-        return <MyScrollView/>
+        // return <MyScrollView/>
+        return <BannerView/>
     }
 }
