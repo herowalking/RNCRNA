6fffd9b0850bedd34577637b8c7411f707b875c4
herowalking <hero@2017>
2018-05-19 21:26:26 +0800

    ScrollView 滚动视图组件

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 
 const BagView = require('./components/BagView');
 const LoginView = require('./components/LoginView');
+const MyScrollView = require('./components/MyScrollView');
 
 export default class first extends Component {
     render() {
         // return <BagView />
-        return <LoginView/>
+        // return <LoginView/>
+        return <MyScrollView/>
     }
 }
