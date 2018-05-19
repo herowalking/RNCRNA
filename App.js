194985193725f00e327056fe90e99fc2f9817dbd
herowalking <hero@2017>
2018-05-19 21:21:42 +0800

    展示远程图片

--- App.js
+++ App.js
@@ -1,50 +1,9 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import React, {Component} from 'react';
 
-var BASE_URL = 'https://api.github.com/repos/facebook/react-native/events';
-console.disableYellowBox = true;
-console.warn('YellowBox is disabled.');
+const BagView = require('./components/BagView');
 
-export default class FetchDemo extends Component {
-
-    getEvent() {
-        // 取得 BASE_URL response 的資料
-        fetch(BASE_URL)
-            .then((res) => res.json())
-            .then((responseData) => {
-                // 單純只做 log
-                console.log(responseData);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .done();
-    }
-
+export default class first extends Component {
     render() {
-        // getEvent 只會 log 出得到的 data, 不會有任何 render
-        this.getEvent();
-
-        return (
-            <View style={styles.container}>
-                <Text>
-                    Welcome to React Native!
-                </Text>
-            </View>
-        );
+        return <BagView />
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    }
-})
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+#### 开发环境：
+夜神模拟器  + expo + CRNA(create-react-native-app)
+npm install -g create-react-native-app
+create-react-native-app HelloCRNA
+cd HelloCRNA
+npm start
+
+如果模拟器连接不上：
+1.下载夜神模拟器最新版
+http://www.yeshen.com/
+2.打开夜神模拟器
+3.打开cmd,执行以下代码连接设备
+ adb connect 127.0.0.1:62001
+
+后台接口：
+切到file_server目录运行 node app.js
+
+---
+
 This project was bootstrapped with [Create React Native App](https://github.com/react-community/create-react-native-app).
 
 Below you'll find information about performing common tasks. The most recent version of this guide is available [here](https://github.com/react-community/create-react-native-app/blob/master/react-native-scripts/template/README.md).
