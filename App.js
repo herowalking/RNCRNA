27e22689c6b2f61de768cceb22e419449c04898f
Lincoln <lincolnpb@hotmail.com>
2018-05-23 16:47:15 +0800

    修改选项卡背景颜色

--- App.js
+++ App.js
@@ -1,35 +1,92 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { FlatList, StyleSheet, Text, View, Button } from 'react-native';
+import { createMaterialTopTabNavigator } from 'react-navigation';
 
-const HomeScreen = () => (
+const dynamicScreen = () => (
+    <View style={styles.container}>
+        <Text style={styles.news}>新闻列表</Text>
+        <FlatList
+            data={[
+                {key: 'Devin'},
+                {key: 'Jackson'},
+                {key: 'James'},
+                {key: 'Joel'},
+                {key: 'John'},
+                {key: 'Lincoln'},
+                {key: 'Jimmy'},
+                {key: 'Kate'},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+    </View>
+);
+
+const FindScreen = () => (
     <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-        <Text>Home Screen</Text>
+        <Text>Find Screen</Text>
     </View>
 )
 
-const ProfileScreen = () => (
+const MyScreen = () => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
+        <Text>My Screen</Text>
     </View>
 )
 
 
-const RootTabs = createBottomTabNavigator(
+const RootTabs = createMaterialTopTabNavigator(
     {
-        Home: {
-            screen: HomeScreen
+        dynamic: {
+            screen: dynamicScreen,
+            navigationOptions: ({navigation}) => ({
+                title: '动态',
+            }),
+        },
+        find: {
+            screen: FindScreen,
+            navigationOptions: ({navigation}) => ({
+                title: '发现'
+            })
         },
         Profile: {
-            screen: ProfileScreen
+            screen: MyScreen,
+            navigationOptions: ({navigation}) => ({
+                title: '我的'
+            })
         }
     },
     {
         tabBarPosition: 'bottom',
         animationEnabled: true,
         tabBarOptions: {
-            activeTintColor: '#fff'
+            activeTintColor: '#28a745',
+            inactiveTintColor: "#232323",
+            style: {
+                backgroundColor: '#F2F2F2'
+            },
+            indicatorStyle: {
+                height:0
+            }
         }
     });
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        color: '#232323',
+    },
+    news: {
+        fontSize:20,
+        paddingLeft:10,
+        borderBottomColor:'#dadada',
+        borderBottomWidth:1,
+        justifyContent: 'center'
+    }
+});
 
 export default RootTabs;
