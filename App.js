e812313b09ffae0c1b9f5db33bc543fff7f40389
Lincoln <lincolnpb@hotmail.com>
2018-06-01 14:34:06 +0800

    justifyContent可以决定其子元素沿着主轴的排列方式。

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class JustifyContentBasics extends Component {
     render() {
         return (
             // 尝试把`row`改为`column`看看
-            <View style={{flex:1, flexDirection:'column'}}>
+            <View style={{flex:1, flexDirection:'column', justifyContent:'flex-end'}}>
                 <View style={{width:50, height:50, backgroundColor:'powderblue'}} />
                 <View style={{width:50, height:50, backgroundColor:'skyblue'}} />
                 <View style={{width:50, height:50, backgroundColor:'steelblue'}} />
